1abf64bd6741b04f633017672faf2263d4aeb9d4
yportne <yportne@163.com>
2021-04-06 01:44:33 +0800

    feat(matrix mult):add *- operation

--- src/main/scala/src/Matrix.scala
+++ src/main/scala/src/Matrix.scala
@@ -8,7 +8,7 @@ case class Matrix2D(
   m : Int,
   n : Int
 ) extends ImplicitArea[Vec[Vec[SInt]]] {
-  val mat = Vec(Vec(SInt(width),n),m)
+  val mat = Vec(Vec(Reg(SInt(width)),n),m)
 
   def apply(range1 : Range, range2 : Range): Vec[Vec[SInt]] = {
     Vec(range1.map(x => Vec(range2.map(y => mat(x)(y)).toList)).toList)
@@ -42,6 +42,22 @@ case class Matrix2D(
     ret
   }
 
+  def *- (that : Matrix2D): Matrix2D = {
+    this.rotateUp
+    val ret = Matrix2D(this.width.value+log2Up(this.n) bits,this.m,that.n)//Vec(SInt(this.width.value+log2Up(this.n) bits),that.n)
+    for(i <- 0 until that.n) {
+      ret(0,i) := (0 until n).map{x =>
+        val ret = RegNext(this(0,x) * that(x,i))
+        ret.setCompositeName(this,"mul_"+that.getClass().getSimpleName()+"_0_"+i+"_"+x,true)
+        //ret.setName(this.getName()+"_mul_"+i+"_"+j,true)
+        //ret.setCompositeName(this,"mul_"+i+"_"+j,true)//.setName(this.getName()+"_mul_"+that.getName()+"_"+i+"_"+j,true)
+        ret(2*width.value - 1 downto width.value)
+      }.reduceBalancedTree(_ +^ _, (s,l) => RegNext(s))
+    }
+    ret.shiftDown
+    ret
+  }
+
   def init(value : Int): Unit = {
     for(i <- 0 until m) {
       for(j <- 0 until n) {
@@ -63,16 +79,42 @@ case class Matrix2D(
     }
     ret
   }
-  //def  (that : Matrix2D): Matrix2D = {
-  //  for(i <- 0 until ) {
-  //    this() := this.mat()
-  //  }
-  //  val ret = Vec()
-  //  for(i <- 0 until ) {
-  //    ret() := 
-  //  }
-  //  ret
-  //}
+  
+
+  def shiftDown = {
+    for(i <- 0 until m-1) {
+      this(i+1,0 until n) := this(i,0 until n)
+    }
+  }
+  def shiftRight = {
+    for(i <- 0 until n-1) {
+      this(0 until m,i+1) := this(0 until m,i)
+    }
+  }
+  def rotateUp = {
+    for(i <- 0 until m-1) {
+      this(i,0 until n) := this(i+1,0 until n)
+    }
+    this(m-1,0 until n) := this(0,0 until n)
+  }
+  def rotateDown = {
+    for(i <- 0 until m-1) {
+      this(i+1,0 until n) := this(i,0 until n)
+    }
+    this(0,0 until n) := this(m-1,0 until n)
+  }
+  def rotateLeft = {
+    for(i <- 0 until n-1) {
+      this(0 until m,i) := this(0 until m,i+1)
+    }
+    this(0 until m,n-1) := this(0 until m,0)
+  }
+  def rotateRight = {
+    for(i <- 0 until n-1) {
+      this(0 until m,i+1) := this(0 until m,i)
+    }
+    this(0 until m,0) := this(0 until m,n-1)
+  }
 
   override def implicitValue: Vec[Vec[SInt]] = this.mat
 
--- src/main/scala/src/testMatrix.scala
+++ src/main/scala/src/testMatrix.scala
@@ -6,11 +6,17 @@ import spinal.lib.Matrix._
 class testMatrix extends Component {
   val m1 = Matrix2D(8 bits,2,2)
   val m2 = Matrix2D(8 bits,2,2)
+  m2.rotateLeft
   //val m3 = m1 * m2
-  m1 := (m1 * m2)(8 downto 1)
+  //m1 := (m1 * m2)(8 downto 1)
   val m3 = Matrix2D(8 bits,2,2)
   m3(0,0 until 2) := m1(0,0 until 2)
   m3(1,0 until 2) := m2(1,0 until 2)
+  val m4 = Matrix2D(9 bits,2,2)
+  m4 := m1 *- m2
+
+  val m5 = m1 * m2
+  //m4.shiftDown
 }
 
 object test {
